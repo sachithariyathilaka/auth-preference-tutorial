89fd4e6b960069a2b25fdd30171cc0f8e7b87f1c
Sachith Ariyathilaka <sachith.vidu@yahoo.com>
2024-05-28 15:28:51 +0530

    Initial screen developed

--- app/src/main/java/io/github/sachithariyathilaka/MainActivity.java
+++ app/src/main/java/io/github/sachithariyathilaka/MainActivity.java
@@ -1,6 +1,8 @@
 package io.github.sachithariyathilaka;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setUserStatus();
+    }
+
+    private void setUserStatus() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.user_status, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner userStatus = (Spinner) findViewById(R.id.userStatus);
+        userStatus.setAdapter(adapter);
     }
 }
